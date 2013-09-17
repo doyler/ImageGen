dd2b5e120dc31e05ccb395f11ea2147738ab22dc
doyler <dev@doyler.net>
2013-09-17 15:00:14 -0400

    2x2 with int to string to binary counting

--- ImageGen.py
+++ ImageGen.py
@@ -1,21 +1,14 @@
 import os, glob
 from PIL import Image, ImageDraw
 
-img = Image.new('1', (3, 3), "white")
+img = Image.new('1', (2, 2), "white")
 myImg = []
 
 for filename in glob.glob("*.png"):
     os.remove(filename)
 
-for i in range (0,2):
-    for j in range (0,2):
-        for k in range (0,2):
-            for l in range (0,2):
-                for m in range (0,2):
-                    for n in range (0,2):
-                        for o in range (0,2):
-                            for p in range (0,2):
-                                for q in range (0,2):
-                                    myImg = [i*255,j*255,k*255,l*255,m*255,n*255,o*255,p*255,q*255]
-                                    img.putdata(myImg)
-                                    img.save('test'+str(i)+str(j)+str(k)+str(l)+str(m)+str(n)+str(o)+str(p)+str(q)+'.png')
+for i in range (0,16):
+    bin = "{:04b}".format(i)
+    myImg = [int(bin[0])*255,int(bin[1])*255,int(bin[2])*255,int(bin[3])*255]
+    img.putdata(myImg)
+    img.save('test'+bin+'.png')
