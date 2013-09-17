e8065d2be62228f23482d072112d94ac5c29e987
doyler <dev@doyler.net>
2013-09-17 15:44:18 -0400

    Binary counting, dynamic strings

--- ImageGen.py
+++ ImageGen.py
@@ -1,4 +1,4 @@
-import os, glob
+import os, glob, math
 from PIL import Image, ImageDraw
 
 img = Image.new('1', (2, 2), "white")
@@ -8,7 +8,10 @@ for filename in glob.glob("*.png"):
     os.remove(filename)
 
 for i in range (0,16):
-    bin = "{:04b}".format(i)
-    myImg = [int(bin[0])*255,int(bin[1])*255,int(bin[2])*255,int(bin[3])*255]
-    img.putdata(myImg)
-    img.save('test'+bin+'.png')
+    format = "{:0"+str(int(math.ceil(math.log(16,2))))+"b}"
+    bin = format.format(i)
+    myImg = []
+    for j in range (0,len(bin)):
+        myImg.append(int(bin[j])*255)
+        img.putdata(myImg)
+        img.save('test'+bin+'.png')
