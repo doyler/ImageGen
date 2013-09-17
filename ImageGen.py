025393a09cf94aa81d78ede3be9591a8fd140e88
doyler <dev@doyler.net>
2013-09-17 15:49:25 -0400

    Custom size (still a square)

--- ImageGen.py
+++ ImageGen.py
@@ -1,14 +1,17 @@
 import os, glob, math
 from PIL import Image, ImageDraw
 
-img = Image.new('1', (2, 2), "white")
+side = 2
+max = 2**(side*side)
+
+img = Image.new('1', (side, side), "white")
 myImg = []
 
 for filename in glob.glob("*.png"):
     os.remove(filename)
 
-for i in range (0,16):
-    format = "{:0"+str(int(math.ceil(math.log(16,2))))+"b}"
+for i in range (0,max):
+    format = "{:0"+str(int(math.ceil(math.log(max,2))))+"b}"
     bin = format.format(i)
     myImg = []
     for j in range (0,len(bin)):
